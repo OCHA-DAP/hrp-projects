d634a0cde12ed7b17cd2ec88cded8a8455f2df6e
David Megginson <david.megginson@gmail.com>
2020-04-22 11:25:59 -0400

    Added Quick Charts and cleaned up code.

--- make-hrp-datasets.py
+++ make-hrp-datasets.py
@@ -1,13 +1,19 @@
 import config
-import ckanapi, datetime, json, logging, requests, urllib
+import ckanapi, datetime, json, logging, re, requests, urllib
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("HPC projects")
+
+
+
+########################################################################
+# Constants
+########################################################################
 
 PLANS_URL = "https://api.hpc.tools/v2/public/plan"
 CKAN_PATTERN = "hrp-projects-{iso3}"
 HXL_PATTERN = "https://proxy.hxlstandard.org/data/93fb54.csv?url={api_url}"
-API_PATTERN = "https://api.hpc.tools/v2/public/project/search?planCodes={code}&excludeFields=locations,governingEntities,targets"
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("HPC projects")
+API_PATTERN = "https://api.hpc.tools/v2/public/project/search?planCodes={code}&excludeFields=locations,governingEntities,targets&limit=100000"
 
 
 
@@ -21,7 +27,7 @@ plans_data = dict()
 countries_data = dict()
 """ Information about countries found """
 
-dataset_date = "01/01/2015-12/31/{}".format(datetime.date.today().year)
+dataset_date = "01/01/{}-12/31/{}".format(config.CONFIG['cutoff_year'], datetime.date.today().year)
 """ The dataset goes to the end of the current year """
 
 
@@ -32,7 +38,7 @@ dataset_date = "01/01/2015-12/31/{}".format(datetime.date.today().year)
 
 def scan_plans ():
     """ Scan all of the plans current on hpc.tools and sort into countries
-    Include only plans from 2015 or later
+    Include only plans from config.CONFIG['cutoff_year'] or later
 
     Side effect: Uses global variables plans_data and countries_data
     """
@@ -41,9 +47,12 @@ def scan_plans ():
         data = response.json()['data']
         for plan in data:
 
+            plan_code = plan['planVersion']['code']
+
             # skip if it's not an HPC project
-            if not plan['planVersion'].get('isForHPCProjects'):
-                continue
+            # if not plan['planVersion'].get('isForHPCProjects'):
+            #     logger.info("Skipping %s (not HPC)", plan_code)
+            #     continue
 
             # skip if there's no country ISO3
             iso3 = None
@@ -54,21 +63,31 @@ def scan_plans ():
                         countries_data[iso3] = location.get("name")
                     break
             if iso3 is None:
+                logger.info("Skipping %s (no country code)", plan_code)
                 continue
 
-            # skip if it's from before 2015
+            # skip if it's from before the cutoff year
             has_recent = False
             for year in plan["years"]:
-                if "year" in year and int(year["year"]) >= 2015:
+                if "year" in year and int(year["year"]) >= config.CONFIG['cutoff_year']:
                     has_recent = True
             if not has_recent:
+                logger.info("Skipping %s (before %d)", plan_code, config.CONFIG['cutoff_year'])
                 continue
+
+            # skip if it doesn't have any projects
+            with requests.get(API_PATTERN.format(code=plan_code)) as response:
+                logger.info("Checking projects for %s...", plan_code)
+                data = response.json()
+                if len(data['data']['results']) < 1:
+                    logger.info("Skipping %s (no projects)", plan_code)
+                    continue
 
             # OK, this plan is a keeper
             if iso3 not in plans_data:
                 plans_data[iso3] = list()
             plans_data[iso3].append({
-                "code": plan['planVersion'].get('code'),
+                "code": plan_code,
                 "name": plan['planVersion'].get('name'),
                 "start": plan['planVersion'].get('startDate'),
                 "end": plan['planVersion'].get('endDate'),
@@ -80,7 +99,7 @@ def make_dataset (iso3, plans):
     """ Create or replace the HDX dataset for the country """
 
     dataset_id = CKAN_PATTERN.format(iso3=iso3.lower())
-    country_name = countries_data[iso3]
+    country_name = re.sub(", .*$", "", countries_data[iso3])
 
     # Fill in full CKAN package except for resources
     package = {
@@ -98,20 +117,20 @@ def make_dataset (iso3, plans):
         "dataset_source": "HPC Tools",
         "tags": [
             {
-                "vocabulary_id": "Topics",
+                "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
                 "name": "activities - projects",
             },
             {
-                "vocabulary_id": "Topics",
+                "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
                 "name": "hxl",
             },
             {
-                "vocabulary_id": "Topics",
+                "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
                 "name": "who is doing what and where - 3w - 4w - 5w",
             },
         ],
         "groups": [
-            {"name": iso3}
+            {"name": iso3.lower()}
         ],
         "has_quickcharts": True,
         "owner_org": "ocha-fts",
@@ -119,7 +138,7 @@ def make_dataset (iso3, plans):
         "notes": "Projects proposed, in progress, or completed as part of the annual {name} Humanitarian Response Plans (HRPs) or other Humanitarian Programme Cycle plans. The original data is available on https://hpc.tools\r\n\r\nNote that some projects are not publicly listed, due to security or personal-privacy concerns.".format(
             name=country_name
         ),
-        "title": "Humanitarian Programme Cycle projects for {name}".format(
+        "title": "Humanitarian Response Plan projects for {name}".format(
             name=country_name
         )
     }
@@ -168,6 +187,18 @@ def save_dataset (ckan, package):
         ckan.call_action('package_create', package)
         logger.info("Created %s...", package['name'])
 
+def add_quickcharts (ckan, package_id):
+    package = ckan.action.package_show(id=package_id)
+    resource_id = package['resources'][0]['id']
+    view = {
+        "description": "",
+        "title": "Quick Charts",
+        "resource_id": resource_id,
+        "view_type": "hdx_hxl_preview",
+        "hxl_preview_config": config.CONFIG['quickcharts_config'],
+    }
+    ckan.call_action('resource_view_create', view)
+
 
 
 ########################################################################
@@ -186,6 +217,8 @@ for iso3 in plans_data:
     logger.info("Creating dataset for %s", iso3)
     package = make_dataset(iso3, plans_data[iso3])
     save_dataset(ckan, package)
+    logger.info("Adding Quick Charts for %s", package['name'])
+    add_quickcharts(ckan, package['name'])
 
 exit(0)
     
